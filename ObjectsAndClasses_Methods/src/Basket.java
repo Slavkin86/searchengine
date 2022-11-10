63f6d96321dc5f063ab2e94c01a213b9e90ecb9e
Вячеслав <slavkin86@mail.ru>
2022-11-11 00:44:14 +0400

    Basket completed

--- ObjectsAndClasses_Methods/src/Basket.java
+++ ObjectsAndClasses_Methods/src/Basket.java
@@ -1,11 +1,16 @@
 public class Basket {
 
     private static int count = 0;
-    private String items = "";
-    private int totalPrice = 0;
+    private String items;
+    private int totalPrice;
     private int limit;
+    private double totalWeight;
+
 
     public Basket() {
+        this.items = "";
+        this.totalPrice = 0;
+        this.totalWeight = 0.0;
         increaseCount(1);
         items = "Список товаров:";
         this.limit = 1000000;
@@ -16,55 +21,64 @@ public class Basket {
         this.limit = limit;
     }
 
-    public Basket(String items, int totalPrice) {
+    public Basket(String items, int totalPrice, int totalWeight) {
         this();
         this.items = this.items + items;
         this.totalPrice = totalPrice;
+        this.totalWeight = (double)totalWeight;
     }
 
     public static int getCount() {
+
         return count;
     }
 
     public static void increaseCount(int count) {
-        Basket.count = Basket.count + count;
-    }
 
-    public void add(String name, int price) {
-        add(name, price, 1);
+        Basket.count += count;
     }
 
     public void add(String name, int price, int count) {
+        add(name, price, 1);
+    }
+
+    public void add(String name, int price, int count, double weight) {
         boolean error = false;
         if (contains(name)) {
             error = true;
         }
 
-        if (totalPrice + count * price >= limit) {
+        if (this.totalPrice + count * price >= this.limit) {
             error = true;
         }
 
         if (error) {
             System.out.println("Error occured :(");
             return;
+        } else {
+            this.items = this.items + "\n" + name + " - " + count + " шт. - " + price + "руб. - " + weight * (double)count + "гр.";
+            this.totalPrice += count * price;
+            System.out.println(" Сумма корзины: " + this.getTotalPrice());
+            this.totalWeight += weight * (double)count;
+            System.out.println(" Общий вес корзины: " + this.getTotalWeight());
         }
 
-        items = items + "\n" + name + " - " +
-            count + " шт. - " + price;
-        totalPrice = totalPrice + count * price;
     }
 
     public void clear() {
-        items = "";
-        totalPrice = 0;
+        this.items = "";
+        this.totalPrice = 0;
     }
 
     public int getTotalPrice() {
-        return totalPrice;
+        return this.totalPrice;
+    }
+    public double getTotalWeight() {
+        return this.totalWeight;
     }
 
     public boolean contains(String name) {
-        return items.contains(name);
+        return this.items.contains(name);
     }
 
     public void print(String title) {
--- ObjectsAndClasses_Methods/src/Main.java
+++ ObjectsAndClasses_Methods/src/Main.java
@@ -1,17 +1,23 @@
 public class Main {
-    public Main() {
-    }
     public static void main(String[] args) {
-        boolean a = true;
-        boolean b = true;
-        Arithmetic arithmetic = new Arithmetic();
-        arithmetic.sum();
-        arithmetic.mult();
-        arithmetic.max();
-        arithmetic.min();
-        System.out.println(arithmetic.sum());
-        System.out.println(arithmetic.mult());
-        System.out.println(arithmetic.max());
-        System.out.println(arithmetic.min());
+        Basket basket = new  Basket();
+        basket.add("Хлеб", 25, 5, 15);
+        basket.add("Колбаса", 250, 5, 10);
+        basket.add("Сыр", 350, 15, 30);
+        basket.print("");
+
+
+
+     //   boolean a = true;
+     //   boolean b = true;
+     //   Arithmetic arithmetic = new Arithmetic();
+     //   arithmetic.sum();
+     //   arithmetic.mult();
+     //   arithmetic.max();
+     //   arithmetic.min();
+     //   System.out.println(arithmetic.sum());
+     //   System.out.println(arithmetic.mult());
+     //   System.out.println(arithmetic.max());
+     //   System.out.println(arithmetic.min());
     }
 }
