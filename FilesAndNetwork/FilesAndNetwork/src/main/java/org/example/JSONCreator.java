1e2e61c26c731cf7865fd695f5f7e172ae418717
Вячеслав <slavkin86@mail.ru>
2023-02-01 01:42:55 +0400

    Completed

---
+++ FilesAndNetwork/FilesAndNetwork/src/main/java/org/example/JSONCreator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+
+public class JSONCreator {
+
+    public JSONCreator() {
+    }
+
+    public void writeInJSONFile(JSONObject object, String path) throws Exception {
+        FileWriter file = new FileWriter(path);
+        file.write(object.toJSONString());
+        file.flush();
+        file.close();
+    }
+}
