54604dec54719963a1ddc5c2a00d39e04ab24b37
Вячеслав <slavkin86@mail.ru>
2023-02-02 01:38:22 +0400

    Completed

---
+++ FilesAndNetwork/SQLAndHibernate/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "JDBC:mysql://localhost:3306/skillbox";
+        String user = "root";
+        String pass = "06011986";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, user, pass);
+            Statement statement = connection.createStatement();
+//            statement.execute("SELECT pl.course_name name, pl.subscription_date FROM PurchaseList pl WHERE pl.course_name = \"Веб-разработчик c 0 до PRO\" ORDER BY pl.subscription_date;");
+            ResultSet resultSet = statement.executeQuery("SELECT pl.course_name Course_Name, " +
+                    "(COUNT(pl.course_name) / TIMESTAMPDIFF(MONTH, MIN(subscription_date), MAX(subscription_date))) AVG_value " +
+                    "FROM PurchaseList pl GROUP BY pl.course_name ORDER BY pl.subscription_date;");
+            while (resultSet.next()){
+                String resSet = resultSet.getString("Course_Name");
+                String resSet2 = resultSet.getString("AVG_value");
+                System.out.println(resSet + " - " + resSet2 + " per month");
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
