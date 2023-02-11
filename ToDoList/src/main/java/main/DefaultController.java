8260261afbd4d84d7a08979b408e815c7eaadc40
Вячеслав <slavkin86@mail.ru>
2023-02-12 02:23:37 +0400

    Completed

--- ToDoList/src/main/java/main/DefaultController.java
+++ ToDoList/src/main/java/main/DefaultController.java
@@ -1,14 +1,26 @@
 package main;
 
+import model.ToDoRepository;
+import model.Todo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-@RestController
+@Controller
 public class DefaultController {
     @RequestMapping("/")
-    public String index() {
-        return (new Date()).toString();
+    public String index(Model model) {
+        Iterable<Todo> todoIterable = ToDoRepository.findAll();
+        ArrayList<Todo> todos = new ArrayList<>();
+        for (Todo todo : todoIterable) {
+            todos.add(todo);
+        }
+        model.addAttribute("todos", todos);
+        model.addAttribute("todosCount", todos.size());
+        return "index";
     }
 }
